15097974c33885b4fd944d8455c4f9f20cb21a7b
Kevin <kevin.morales05@epn.edu.ec>
2021-05-19 14:24:46 -0500

    API with Laravel 8

---
+++ database/seeders/ArticlesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Article;
+use Illuminate\Database\Seeder;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+ {
+ // Vaciar la tabla.
+    Article::truncate();
+        $faker = \Faker\Factory::create();
+ // Crear artículos ficticios en la tabla
+        for ($i = 0; $i < 50; $i++) {
+        Article::create([
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+        ]);
+        }
+    }
+}
